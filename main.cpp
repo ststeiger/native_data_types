9848c2bef7c80ce7fb4b936051565dc0d38ef45b
Me <Me@example.com>
2020-02-18 17:17:14 +0100

    Fix compilation on Linux

--- main.cpp
+++ main.cpp
@@ -1,11 +1,65 @@
 
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
+#ifdef __cplusplus
+    // #include <iostream>
+    // #include <fstream>
+    #include <cstdio>
+    #include <cstdlib>
+    #include <cstdint>
+    #include <cstring>
+#else
+    #include <stdio.h> // printf
+    #include <stdlib.h> // for malloc
+    #include <stdint.h> // for int32_t, int8_t, etc.
+    #include <stdbool.h> // true, false in plain old C
+    #include <string.h>  // for strlen, strcopy
+#endif
+
+
+
 
 
 #if ( defined (__WIN32__) || defined (__WIN32) || defined (_WIN32) || defined (WIN32) || defined (__WIN64__) || defined (__WIN64) || defined (_WIN64) || defined (WIN64)  )
     #include <windows.h>
+#else
+    typedef int				 BOOL;
+    typedef long int           LONG;
+    typedef short int          SHORT;
+    typedef char               CHAR;
+
+    typedef unsigned long int  DWORD;
+    typedef unsigned short     WORD;
+    typedef unsigned char      BYTE;
+    typedef unsigned int       UINT;
+
+    typedef long long          INT64;
+    typedef unsigned long long UINT64;
+
+    typedef int                PT_FILEHANDLE;
+    typedef void             * DLL_HANDLE;
+
+    typedef unsigned long      DWORD;
+    typedef unsigned long*     LPDWORD;
+    typedef void*              LPOVERLAPPED;
+    typedef void*              OVERLAPPED;
+    typedef void*              LPVOID;
+    typedef void*              PVOID;
+    typedef void               VOID;
+    typedef int                HANDLE;         // note that handle here is assumed to be
+    // a pointer to a file decriptor
+    typedef int*               PHANDLE;
+    typedef int                BOOL;
+
+    typedef unsigned long      UINT32;
+    typedef unsigned long      ULONG;
+    typedef unsigned short     USHORT;
+    typedef unsigned char      UCHAR;
+    typedef long long          INT64;
+    typedef long long          LARGE_INTEGER;
+    typedef unsigned char      BYTE;
+
+    typedef int64_t LONGLONG;
+    typedef uint64_t ULONGLONG;
+
 #endif
 
 
